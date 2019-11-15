83259f09b948b3424366148643a02ff0d514bbb4
Lucy Mair <lucy.may.mair@outlook.com>
2019-11-15 17:37:28 +0000

    Export Repeat

--- src/time/index.ts
+++ src/time/index.ts
@@ -4,10 +4,12 @@ import { Stretch } from './stretch';
 import { Hook } from './hook';
 import { Delay } from './delay';
 import { Reverse } from './reverse';
+import { Repeat } from './repeat';
 
 export const TimeTransformers = {
   Stretch,
   Hook,
   Delay,
-  Reverse
+  Reverse,
+  Repeat
 };
